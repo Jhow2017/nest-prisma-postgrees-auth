5a20addb86515b71af613f3667d5309a61ec1ede
jhowgama <jonathan.gama@teltecsolutions.com.br>
2025-11-10 19:21:04 -0400

    Update authentication module to include JWT support and modify email in API examples
    - Updated email address in API examples for consistency.
    - Added @nestjs/jwt dependency to package.json and package-lock.json for JWT handling.
    - Integrated JwtModule into AuthModule with configuration for token signing.
    - Created constants file for JWT secret management.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserValidation } from 'src/common/validations/user.validation';
+import { jwtConstants } from './constants';
 
 @Module({
+  imports: [JwtModule.register({
+    global: true,
+    secret: jwtConstants.secret,
+    signOptions: { expiresIn: '60s' },
+  })],
   controllers: [AuthController],
   providers: [AuthService, PrismaService, UserValidation]
 })
