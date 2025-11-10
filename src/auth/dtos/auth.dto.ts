eb035446f56484c2f3663a13b0e0c967457f4697
jhowgama <jonathan.gama@teltecsolutions.com.br>
2025-11-10 19:58:46 -0400

    Refactor authentication DTOs and enhance validation
    - Updated SignupDto and SigninDto to use class-validator for improved validation.
    - Added global validation pipe in main.ts to enforce DTO validation across the application.
    - Included new dependencies class-transformer and class-validator in package.json and package-lock.json.
    - Removed unnecessary blank line in api.http.

--- src/auth/dtos/auth.dto.ts
+++ src/auth/dtos/auth.dto.ts
@@ -1,10 +1,25 @@
-export interface SignupDto {
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class SignupDto {
+    @IsString({ message: 'nome é obrigatório' })
+    @IsNotEmpty({ message: 'nome é obrigatório' })
     name: string;
+
+    @IsEmail({}, { message: 'email inválido' })
+    @IsNotEmpty({ message: 'email é obrigatório' })
     email: string;
+
+    @IsNotEmpty({ message: 'senha é obrigatório' })
+    @IsString({ message: 'senha é obrigatório' })
     password: string;
 }
 
-export interface SigninDto {
+export class SigninDto {
+    @IsEmail({}, { message: 'email inválido' })
+    @IsNotEmpty({ message: 'email é obrigatório' })
     email: string;
+
+    @IsNotEmpty({ message: 'senha é obrigatório' })
+    @IsString({ message: 'senha é obrigatório' })
     password: string;
 }
